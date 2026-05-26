3e8926bcb2e2625db0ce3fa490b6f749cc68142c
degife tise <degifetise09@gmail.com>
2026-05-26 20:44:41 +0300

    Feature pricing section added to with three plan free, pro and entrprise monthly plan

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Pricing from "./components/Pricing";
 function App() {
   return (
     <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
       <Navbar />
       <Hero />
       <Features />
+      <Pricing />
     </div>
   );
 }
